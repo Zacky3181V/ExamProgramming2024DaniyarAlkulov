881e90c99f77030d9df4702e0dd77d9424d47246
Daniyar Alkulov <medvedicabolsaa@gmail.com>
2024-02-14 18:02:31 +0100

    Read button works

--- DaniyarAlkulov/ExpensesManager/Form1.Designer.cs
+++ DaniyarAlkulov/ExpensesManager/Form1.Designer.cs
@@ -28,19 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new System.Windows.Forms.Label();
+            pathBox = new System.Windows.Forms.TextBox();
+            readButton = new System.Windows.Forms.Button();
+            statsButton = new System.Windows.Forms.Button();
+            resultBox = new System.Windows.Forms.TextBox();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(27, 22);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(31, 15);
+            label1.TabIndex = 0;
+            label1.Text = "path";
+            // 
+            // pathBox
+            // 
+            pathBox.Location = new System.Drawing.Point(88, 19);
+            pathBox.Name = "pathBox";
+            pathBox.Size = new System.Drawing.Size(526, 23);
+            pathBox.TabIndex = 1;
+            pathBox.Text = "C:\\Program Files (x86)\\Invoices.txt";
+            pathBox.TextChanged += pathBox_TextChanged;
+            // 
+            // readButton
+            // 
+            readButton.Location = new System.Drawing.Point(23, 76);
+            readButton.Name = "readButton";
+            readButton.Size = new System.Drawing.Size(95, 37);
+            readButton.TabIndex = 2;
+            readButton.Text = "Read";
+            readButton.UseVisualStyleBackColor = true;
+            readButton.Click += readButton_Click;
+            // 
+            // statsButton
+            // 
+            statsButton.Location = new System.Drawing.Point(174, 78);
+            statsButton.Name = "statsButton";
+            statsButton.Size = new System.Drawing.Size(96, 32);
+            statsButton.TabIndex = 3;
+            statsButton.Text = "Stats";
+            statsButton.UseVisualStyleBackColor = true;
+            statsButton.Click += statsButton_Click;
+            // 
+            // resultBox
+            // 
+            resultBox.Location = new System.Drawing.Point(12, 119);
+            resultBox.Multiline = true;
+            resultBox.Name = "resultBox";
+            resultBox.Size = new System.Drawing.Size(776, 319);
+            resultBox.TabIndex = 4;
+            resultBox.TextChanged += resultBox_TextChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(resultBox);
+            Controls.Add(statsButton);
+            Controls.Add(readButton);
+            Controls.Add(pathBox);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pathBox;
+        private System.Windows.Forms.Button readButton;
+        private System.Windows.Forms.Button statsButton;
+        private System.Windows.Forms.TextBox resultBox;
     }
 }
--- DaniyarAlkulov/ExpensesManager/Form1.cs
+++ DaniyarAlkulov/ExpensesManager/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,37 @@ namespace ExpensesManager
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pathBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text = "";
+            var path = pathBox.Text;
+            var content = File.ReadAllLines(path);
+            for(var i = 1; i < content.Length; i++)
+            {
+                var line = content[i];
+                var lineCont = line.Split('|');
+                resultBox.Text += string.Join("\t", lineCont);
+                resultBox.Text += $"{Environment.NewLine}";
+            }
+            
+        }
+
+        private void statsButton_Click(object sender, EventArgs e)
+        {
+            var path = pathBox.Text;
+            var content = File.ReadAllLines(path);
+        }
+
+        private void resultBox_TextChanged(object sender, EventArgs e)
         {
 
         }
